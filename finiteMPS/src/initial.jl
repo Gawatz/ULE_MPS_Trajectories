3d1964b77242c5429c2a1c6af24221bfa3b79a8d
Gawatz <82025032+Gawatz@users.noreply.github.com>
2021-04-11 22:17:23 +0200

    name convention and description
    added some descriptions and changed name convention

--- finiteMPS/src/initial.jl
+++ finiteMPS/src/initial.jl
@@ -1,26 +1,27 @@
 """************************ initialization schemes **************************"""
 
 """
-    finiteMPS(N, d, maxD)
+    randmps(N, d, maxD)
 
-constructs a random finite MPS for a  system with N sites with local physical dimsension
+Constructs a random finite MPS for a system with N sites with local physical dimension
 d and max. bond dimension maxD. Bond dimension will increas dynamically with
-system size but wont exceed maxD. 
+system size but wont exceed maxD. Will return a vector containing all local matrices M[i] and
+a vector containing singular values Cvec[i]:
+
+M[1] - Cvec[1] - M[2] - Cvec[2] - M[3] - ... Cvec[N-1] - M[N] - Cvec[N]
 
 # Arguments
-- N: System size
+- N: system size
 - d: local physical dimension
-- maxD: maximal bond Dimension
+- maxD: maximal bond dimension
 
 return:
-	- MPSvec vector storing M for each site
-	- Cvec vector storing singular Values between site and site+1
-	  so the last entrie of Cvec is basically the norm of the sate
- 	  and of dimension 1 x 1 
-
-
+	- MPSvec: vector storing local matrices M[i] for each site i
+	- Cvec: vector storing singular values between site and site+1
+	  The last entry of Cvec is basically the norm of the sate
+ 	  and is of dimension 1 x 1. 
 """
-function randMPS(N::Int, d::Int, maxD::Int)
+function randmps(N::Int, d::Int, maxD::Int)
 	MPSvec = []
 	Cvec = []
 
@@ -54,7 +55,23 @@ function randMPS(N::Int, d::Int, maxD::Int)
 	return MPSvec, Cvec
 end
 
-function randomBloch(N::Int, maxD::Int)
+
+"""
+    randombloch(N, maxD)
+
+!!!ADD DESCRIPTION!!!
+
+# Arguments
+- N: system size
+- maxD: maximal bond dimension
+
+return:
+	- MPSvec: vector storing local matrices M[i] for each site i
+	- Cvec: vector storing singular values between site and site+1
+	  The last entry of Cvec is basically the norm of the sate
+ 	  and is of dimension 1 x 1. 
+"""
+function randombloch(N::Int, maxD::Int)
 		MPSvec = []
 		C = []
 
@@ -113,20 +130,27 @@ function randomBloch(N::Int, maxD::Int)
 
 		return MPSvec, C
  end
+"""
+    coeftomps(N, Coef)
+!!!ADD DESCRIPTION!!! so far just for local hilbertspace dimension d = 2
 
-# this should be essential turned into a convertion once MPS struct is defined 
-function Coef_to_MPS(N::Int, Coef::Vector{<:Number})
+# Arguments
+- N: system size
+- Coef: Coefficient vector 
+
+return:
+	- MPSvec: vector storing local matrices M[i] for each site i
+"""
+function coeftomps(N::Int, Coef::Vector{<:Number})
 	MPSvec = []
 	
 	tmp = zeros(ComplexF64, 1,size(Coef)[1])
 	tmp[1,:] = Coef
 	for site in 1:N 
-		bout_pre, d_left = size(tmp)
-
-		tmp =  reshape(tmp, bout_pre *2, Int(d_left/2))
 		
+		bout_pre, d_left = size(tmp)
+		tmp =  reshape(tmp, bout_pre *2, Int(d_left/2))
 		F = svd(tmp)
-
 		mps = reshape(F.U, bout_pre, 2, size(F.U)[2])
 
 		tmp = diagm(0=>F.S)*F.V'
